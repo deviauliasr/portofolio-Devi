c460286e21e938f204ad17939f34b30d8a9a2dff
Devi Auliasari <120165422+deviauliasr@users.noreply.github.com>
2023-02-07 05:09:38 +0700

    Java
    Spring-mvc-hibernate

--- Java/src/main/java/com/springboot/repository/MahasiswaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.springboot.model.Mahasiswa;
-
-public interface MahasiswaRepository extends CrudRepository<Mahasiswa, Integer>{
-	
-}
